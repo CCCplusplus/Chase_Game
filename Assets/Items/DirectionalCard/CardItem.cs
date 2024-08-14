d1b6bdeda0c7c4d40d94fa3685bdc6e321a03eaf
Jorgebz27 <zenit15george@gmail.com>
2024-08-14 11:27:04 -0600

    ElementalGun & DIrectionalCard almost done

--- Assets/Items/DirectionalCard/CardItem.cs
+++ Assets/Items/DirectionalCard/CardItem.cs
@@ -1,52 +1,81 @@
 using System.Collections;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using Unity.VisualScripting;
 
 public class CardItem : MonoBehaviour
 {
-    [SerializeField] private AudioClip cardShootSound;
-
-    private bool isPickedUp = false;
+    [SerializeField]
+    public float shootForce = 40f;
+    public Transform holdPosition;
     private GameObject player;
-    private Transform itemPosition;
-    private GameObject hitboxGameObject;
-    private GameObject projectilePrefab;
-    private ParticleSystem hitDustParticles;
+    private bool isHeld = false;
+    private Transform playerTransform;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    void Update()
     {
-        if (other.CompareTag("Runner"))
-        {
-            isPickedUp = true;
-            player = other.gameObject;
-
-            hitDustParticles = hitboxGameObject.GetComponent<ParticleSystem>();
-
-            this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
-            this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
-        }
-    }
-
-    private void Update()
-    {
-        if (isPickedUp && player != null)
+        if (isHeld && player != null)
         {
             var inputActions = player.GetComponent<PlayerInput>().actions;
             if (inputActions["Use-Item"].triggered)
-                Fire();
+                ShootItem();
         }
     }
 
-    private void Fire()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (cardShootSound != null)
-            AudioSource.PlayClipAtPoint(cardShootSound, player.transform.position);
+        if (collision.CompareTag("Chaser") && !isHeld)
+        {
+            PickUpItem(collision.gameObject);
+        }
 
-        if (hitDustParticles != null)
-            hitDustParticles.Play();
+        if(collision.CompareTag("Runner"))
+        {
+            InvertPlayerControls(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
 
-        isPickedUp = false;
+    void PickUpItem(GameObject playerObject)
+    {
+        isHeld = true;
+        player = playerObject;
+        playerTransform = player.transform;
+        transform.SetParent(playerTransform);
 
+        Transform holdPos = playerTransform.Find("HoldPosition");
+        if (holdPos != null)
+        {
+            holdPosition = holdPos;
+            transform.position = holdPosition.position;
+        }
 
+        GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    void ShootItem()
+    {
+        isHeld = false;
+        transform.SetParent(null);
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = false;
+        rb.AddForce(holdPosition.right * shootForce, ForceMode2D.Impulse);
+    }
+
+    void InvertPlayerControls(GameObject hitPlayer)
+    {
+        var playerInput = hitPlayer.GetComponent<PlayerInput>();
+        if (playerInput != null)
+        {
+            // Invertir los controles usando input mappings
+            InputActionMap actionMap = playerInput.actions.FindActionMap("Player");
+
+            // Invertir las entradas de "Move"
+            var moveAction = actionMap.FindAction("Move");
+            if (moveAction != null)
+            {
+                moveAction.ApplyBindingOverride("<Gamepad>/leftStick" /*new InputBinding { processors = "invertVector2(invertX=true)" }*/);
+            }
+        }
     }
 }
