56c6164beea2a272ad4bf26b52af625833ab55d0
Marckycat <marco.villegas.lira@gmail.com>
2024-08-28 21:33:59 -0600

    Cooldown de los portales y Mejora del FallPlatform
    Le puse un Cooldown a los portales para que no sea instantaneo de teletransportarse. El FallPlatform tenia el problema de que al tocarla se desaparecia, lo que hice fue cambiarle de que cuando el runner este encima durante un tiempo se activara la desaparicion.

--- Assets/Items/Portal/PortalRunner.cs
+++ Assets/Items/Portal/PortalRunner.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PortalRunner : MonoBehaviour
 {
     private HashSet<GameObject> portalObjects = new HashSet<GameObject>();
+    private Dictionary<GameObject, bool> teleportCooldowns = new Dictionary<GameObject, bool>();
 
     [SerializeField] private Transform destination;
     [SerializeField] private AudioSource portalSound;
+    [SerializeField] private float cooldownTime = 4.5f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (portalObjects.Contains(collision.gameObject))
+        if (teleportCooldowns.ContainsKey(collision.gameObject)&&teleportCooldowns[collision.gameObject])
         {
             return;
         }
@@ -30,12 +32,35 @@ public class PortalRunner : MonoBehaviour
             {
                 destinationPortal.portalObjects.Add(collision.gameObject);
             }
+            //Teletransportar el objeto al destino
             collision.transform.position = destination.position;
+
+            //Iniciar el cooldown para este objeto
+            StartCoroutine(TeleportCooldown(collision.gameObject));
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         portalObjects.Remove(collision.gameObject);
+    }
+
+    private IEnumerator TeleportCooldown(GameObject teleportObject)
+    {
+        //Iniciar el cooldown
+        if (!teleportCooldowns.ContainsKey(teleportObject))
+        {
+            teleportCooldowns.Add(teleportObject, true);
+        }
+        else
+        {
+            teleportCooldowns[teleportObject] = true;
+        }
+
+        //Esperar el tiempo de cooldown
+        yield return new WaitForSeconds(cooldownTime);
+
+        //Finalizar el cooldown
+        teleportCooldowns[teleportObject] = false;
     }
 }
