7f034fde006eac52544e964d6c7cf62015410c26
Marckycat <marco.villegas.lira@gmail.com>
2024-09-05 18:23:51 -0600

    Indicacion visual del Platform
    Hice que cuando el runner o el chaser, cuando toque la plataforma este parpade antes de que desaparezca

--- Assets/Traps/FallingPlatform/FallPlatform.cs
+++ Assets/Traps/FallingPlatform/FallPlatform.cs
@@ -1,14 +1,17 @@
 using System.Collections;
-//using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class FallPlatform : MonoBehaviour
+public class FallPlatform : NetworkBehaviour
 {
     [SerializeField] private float tiempoEspera = 1f;
     [SerializeField] private float tiempoReaparicion = 2f;
+    [SerializeField] private int numeroParpadeos = 3;
+    [SerializeField] private float duracionParpadeo = 0.2f;
+
     private Collider2D col2D;
     private Renderer platformRd;
-    //private bool runnerEncima = false;
 
     private void Start()
     {
@@ -20,20 +23,42 @@ public class FallPlatform : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Runner") || other.gameObject.CompareTag("Chaser"))
         {
-            StartCoroutine(DesapareceYReaparece());
+            if (NetworkClient.active && isServer)
+            {
+                RpcDesapareceYReaparece();
+            }
         }
+    }
+
+    [ClientRpc]
+    private void RpcDesapareceYReaparece()
+    {
+        StartCoroutine(DesapareceYReaparece());
     }
 
     private IEnumerator DesapareceYReaparece()
     {
-        //Espera antes de que la plataforma desaparezca
+
+        // Parpadeo de la plataforma
+        for (int i = 0; i < numeroParpadeos; i++)
+        {
+            platformRd.enabled = false;
+            yield return new WaitForSeconds(duracionParpadeo);
+            platformRd.enabled = true;
+            yield return new WaitForSeconds(duracionParpadeo);
+        }
+
+        // Espera antes de que la plataforma desaparezca
         yield return new WaitForSeconds(tiempoEspera);
-        //Desactiva la plataforma
+
+        // Desactiva la plataforma (desaparece)
         col2D.enabled = false;
         platformRd.enabled = false;
-        //Espera un tiempo antes de reaparecer la plataforma
+
+        // Espera un tiempo antes de reaparecer la plataforma
         yield return new WaitForSeconds(tiempoReaparicion);
-        //Reactiva la plataforma
+
+        // Reactiva la plataforma (reaparece)
         col2D.enabled = true;
         platformRd.enabled = true;
     }
