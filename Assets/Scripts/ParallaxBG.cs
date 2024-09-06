36ee94de0e16d0f88effe1a8fdfba2dcd6d0e3a7
Lug00 <luis.lugo@saei.mx>
2024-09-05 21:20:40 -0600

    BG update

--- Assets/Scripts/ParallaxBG.cs
+++ Assets/Scripts/ParallaxBG.cs
@@ -22,19 +22,18 @@ public class ParallaxBG : MonoBehaviour
     {
         float deltaX = (cameraTransform.position.x - previousCameraPos.x) * parallaxMultiplier;
         float moveAmount = cameraTransform.position.x * (1 - parallaxMultiplier);
-        
         transform.Translate(new Vector3 (deltaX, 0, 0));
         previousCameraPos = cameraTransform.position;
 
-        if(moveAmount > startPos + spriteWidth)
-        {
-          transform.Translate(new Vector3(spriteWidth, 0, 0));
-          startPos += spriteWidth;
-        }
-        else if(moveAmount < startPos - spriteWidth)
-        {
-          transform.Translate(new Vector3(-spriteWidth, 0, 0));
-          startPos -= spriteWidth;
-        }
+        //if(moveAmount > startPos + spriteWidth)
+        //{
+        //  transform.Translate(new Vector3(spriteWidth, 0, 0));
+        //  startPos += spriteWidth;
+        //}
+        //else if(moveAmount < startPos - spriteWidth)
+        //{
+        //  transform.Translate(new Vector3(-spriteWidth, 0, 0));
+        //  startPos -= spriteWidth;
+        //}
     }
 }
