7fe072bb3480356192e0f89b5af77757262a57fe
Lug00 <luis.lugo@saei.mx>
2024-09-13 03:28:10 -0600

    Otra vez Creditos y Animaciones de checkpoints

--- Assets/Scripts/Creditos.cs
+++ Assets/Scripts/Creditos.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
+using UnityEngine.UI;
 public class Creditos : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("WaitToEnd", 60);
+        Invoke("WaitToEnd", 156);
     }
 
     // Update is called once per frame
@@ -18,6 +19,10 @@ public class Creditos : MonoBehaviour
         {
             SceneManager.LoadScene("Main Menu");
         }
+    }
+    public void ReturnRoMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
     }
 
     private void WaitToEnd()
